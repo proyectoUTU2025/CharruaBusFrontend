d6d6b5ee342579aaf1639cdaf2d987a7aec6290d
bentanic-0 <bentanico@gmail.com>
2025-05-29 08:00:02 -0300

    Add search filter by name in ConfiguracionDelSistemaComponent

--- src/app/component/configuracion-del-sistema/configuracion-del-sistema.component.ts
+++ src/app/component/configuracion-del-sistema/configuracion-del-sistema.component.ts
@@ -1,11 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MatTableModule, MatTableDataSource } from '@angular/material/table';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialogModule, MatDialog } from '@angular/material/dialog';
-import { MatPaginatorModule, PageEvent } from '@angular/material/paginator'; // Importa paginator
+import { MatPaginatorModule, PageEvent } from '@angular/material/paginator';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 import { Configuracion } from '../../models/configuracion';
 import { ConfiguracionDelSistemaService } from '../../services/configuracion-del-sistema.service';
@@ -18,12 +21,15 @@ import { EditConfiguracionDialogComponent } from './dialogs/edit-configuracion-d
     styleUrls: ['./configuracion-del-sistema.component.scss'],
     imports: [
         CommonModule,
+        FormsModule,
         ReactiveFormsModule,
         MatTableModule,
         MatIconModule,
         MatButtonModule,
         MatDialogModule,
         MatPaginatorModule,
+        MatFormFieldModule,
+        MatInputModule,
         EditConfiguracionDialogComponent
     ]
 })
@@ -36,6 +42,9 @@ export class ConfiguracionDelSistemaComponent implements OnInit {
     pageSize = 20;
     currentPage = 0;
 
+    // Filtro por nombre
+    filtroNombre: string = '';
+
     constructor(
         private service: ConfiguracionDelSistemaService,
         private dialog: MatDialog
@@ -46,12 +55,22 @@ export class ConfiguracionDelSistemaComponent implements OnInit {
     }
 
     load(page = 0, size = 20) {
-        this.service.list(undefined, page, size).subscribe(pageData => {
+        // Pasamos filtro al service
+        this.service.list(this.filtroNombre?.trim() || undefined, page, size).subscribe(pageData => {
             this.dataSource.data = pageData.content;
             this.totalElements = pageData.totalElements;
             this.pageSize = size;
             this.currentPage = page;
         });
+    }
+
+    aplicarFiltro() {
+        this.load(0, this.pageSize);
+    }
+
+    limpiarFiltro() {
+        this.filtroNombre = '';
+        this.load(0, this.pageSize);
     }
 
     pageChange(event: PageEvent) {

69a3056da1cdcde032c092470ab9fb3c5c35361b
bentanic-0 <bentanico@gmail.com>
2025-05-29 01:42:02 -0300

    Add route for ConfiguracionDelSistemaComponent

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,6 +6,7 @@ import { UsersPageComponent } from './component/users-page/users-page.component'
 import { BusesPageComponent } from './component/buses-page/buses-page.component';
 import { LocalidadesPageComponent } from './component/localidades-page/localidades-page.component';
 import { ViajesPageComponent } from './component/viajes-page/viajes-page.component';
+import { ConfiguracionDelSistemaComponent } from './component/configuracion-del-sistema/configuracion-del-sistema.component';
 import { AuthGuard } from './core/auth/auth.guard';
 
 export const routes: Routes = [
@@ -19,7 +20,8 @@ export const routes: Routes = [
       { path: '', component: UsersPageComponent },
       { path: 'omnibus', component: BusesPageComponent },
       { path: 'localidades', component: LocalidadesPageComponent },
-      { path: 'viajes', component: ViajesPageComponent }
+      { path: 'viajes', component: ViajesPageComponent },
+      { path: 'configuracion', component: ConfiguracionDelSistemaComponent },
     ]
   },
   { path: '**', redirectTo: '' }

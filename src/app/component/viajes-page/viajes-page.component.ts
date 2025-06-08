6a8a289eead91b9d0cca6f1dab284aa65cefe277
bentanic-0 <bentanico@gmail.com>
2025-06-08 16:08:08 -0300

    Enhance viajes page component by adding HTTP client module and improving error handling in data fetching

--- src/app/component/viajes-page/viajes-page.component.ts
+++ src/app/component/viajes-page/viajes-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 import { MatTableModule, MatTableDataSource } from '@angular/material/table';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
@@ -9,8 +10,9 @@ import { MatNativeDateModule } from '@angular/material/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+
 import { ViajeService } from '../../services/viaje.service';
-import { Viaje } from '../../models/viajes';
+import { Viaje } from '../../models';
 import { ExpresoDetailsDialogComponent } from './dialogs/expreso-details-dialog/expreso-details-dialog.component';
 
 @Component({
@@ -18,6 +20,7 @@ import { ExpresoDetailsDialogComponent } from './dialogs/expreso-details-dialog/
   standalone: true,
   imports: [
     CommonModule,
+    HttpClientModule,
     MatTableModule,
     MatFormFieldModule,
     MatInputModule,
@@ -33,20 +36,37 @@ import { ExpresoDetailsDialogComponent } from './dialogs/expreso-details-dialog/
   styleUrls: ['./viajes-page.component.scss']
 })
 export class ViajesPageComponent implements OnInit {
-  columns = ['id', 'fechaSalida', 'fechaLlegada', 'horaSalida', 'horaLlegada', 'origen', 'destino', 'precio', 'pasajesVendibles', 'tipo', 'venta'];
+  columns = [
+    'id',
+    'fechaSalida',
+    'fechaLlegada',
+    'horaSalida',
+    'horaLlegada',
+    'origen',
+    'destino',
+    'precio',
+    'pasajesVendibles',
+    'tipo',
+    'venta'
+  ];
   dataSource = new MatTableDataSource<Viaje>();
 
-  constructor(private viajeService: ViajeService, private dialog: MatDialog) {}
+  constructor(private viajeService: ViajeService, private dialog: MatDialog) { }
 
-ngOnInit(): void {
-  this.viajeService.getAll().subscribe(viajes => {
-    this.dataSource.data = viajes;
-  });
-}
+  ngOnInit(): void {
+    this.viajeService.getAll().subscribe({
+      next: (viajes) => {
+        this.dataSource.data = viajes;
+      },
+      error: (err) => {
+        console.error('Error al cargar viajes desde el backend:', err);
+      }
+    });
+  }
 
-openCrearViajeExpreso(): void {
-  this.dialog.open(ExpresoDetailsDialogComponent, {
-    width: '600px'
-  });
-}  
+  openCrearViajeExpreso(): void {
+    this.dialog.open(ExpresoDetailsDialogComponent, {
+      width: '600px'
+    });
+  }
 }

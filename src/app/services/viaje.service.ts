b7d0a9789eccc1cd6e36a3ba8f747cd70fec75a0
bentanic-0 <bentanico@gmail.com>
2025-06-08 16:14:27 -0300

    Refactor ViajeService to use HttpClient for API interactions and implement pagination for viaje retrieval

--- src/app/services/viaje.service.ts
+++ src/app/services/viaje.service.ts
@@ -1,58 +1,45 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, Observable } from 'rxjs';
-import { Viaje } from '../models/viajes';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Viaje } from '../models';
 
-@Injectable({ providedIn: 'root' })
+@Injectable({
+  providedIn: 'root'
+})
 export class ViajeService {
-  private _viajes = new BehaviorSubject<Viaje[]>([
-    {
-      id: 1,
-      fechaSalida: '15/06/2023',
-      fechaLlegada: '15/06/2023',
-      horaSalida: '08:00',
-      horaLlegada: '12:30',
-      origen: 'Montevideo',
-      destino: 'Punta del Este',
-      precio: 1200,
-      asientos: 45,
-      tipo: 'Regular',
-      venta: 'Disponible'
-    },
-    {
-      id: 2,
-      fechaSalida: '16/06/2023',
-      fechaLlegada: '16/06/2023',
-      horaSalida: '09:00',
-      horaLlegada: '14:00',
-      origen: 'Montevideo',
-      destino: 'Colonia',
-      precio: 950,
-      asientos: 40,
-      tipo: 'Regular',
-      venta: 'Disponible'
-    },
-    {
-      id: 3,
-      fechaSalida: '17/06/2023',
-      fechaLlegada: '17/06/2023',
-      horaSalida: '07:30',
-      horaLlegada: '13:00',
-      origen: 'Punta del Este',
-      destino: 'Montevideo',
-      precio: 1200,
-      asientos: 45,
-      tipo: 'Regular',
-      venta: 'No disponible'
-    }
-  ]);
+  private readonly baseUrl = '/api/viajes';
+
+  constructor(private http: HttpClient) { }
 
   getAll(): Observable<Viaje[]> {
-    return this._viajes.asObservable();
+    return this.http.get<Viaje[]>(this.baseUrl);
   }
 
-  addViaje(viaje: Viaje): void {
-    const viajes = this._viajes.value;
-    const nuevo = { ...viaje, id: viajes.length + 1 };
-    this._viajes.next([...viajes, nuevo]);
+
+  getPage(
+    page: number,
+    size: number,
+    filtros?: { fechaSalida?: string; origen?: string; destino?: string }
+  ): Observable<any> {
+    let params = new HttpParams()
+      .set('page', page.toString())
+      .set('size', size.toString());
+
+    if (filtros?.fechaSalida) {
+      params = params.set('fechaSalida', filtros.fechaSalida);
+    }
+    if (filtros?.origen) {
+      params = params.set('origen', filtros.origen);
+    }
+    if (filtros?.destino) {
+      params = params.set('destino', filtros.destino);
+    }
+
+    return this.http.get<any>(this.baseUrl, { params });
+  }
+
+
+  addViaje(viaje: Viaje): Observable<Viaje> {
+    return this.http.post<Viaje>(this.baseUrl, viaje);
   }
 }

f143384234038263c065673cd7306473a734fa90
bentanic-0 <bentanico@gmail.com>
2025-06-13 17:35:41 -0300

    Refactor routing structure to improve organization and add new authentication and user profile routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,29 +1,37 @@
 import { Routes } from '@angular/router';
-import { LoginPageComponent } from './component/login-page/login-page.component';
-import { SignupPageComponent } from './component/signup-page/signup-page.component';
-import { VerificarCodigoComponent } from './component/signup-page/verificar-codigo/verificar-codigo.component';
-import { UsersPageComponent } from './component/users-page/users-page.component';
-import { BusesPageComponent } from './component/buses-page/buses-page.component';
-import { LocalidadesPageComponent } from './component/localidades-page/localidades-page.component';
-import { ViajesPageComponent } from './component/viajes-page/viajes-page.component';
-import { BusDetailComponent } from './component/buses-page/bus-detail/bus-detail.component';
+import { LoginPageComponent } from './component/auth/login-page/login-page.component';
+import { SignupPageComponent } from './component/auth/signup-page/signup-page.component';
+import { VerifyResetCodeComponent } from './component/auth/verify-reset-code-page/verify-reset-code.component';
+
+import { UsersPageComponent } from './component/user/users-page/users-page.component';
+import { BusesPageComponent } from './component/transport/buses-page/buses-page.component';
+import { LocalidadesPageComponent } from './component/transport/localidades-page/localidades-page.component';
+import { ViajesPageComponent } from './component/transport/viajes-page/viajes-page.component';
+import { BusDetailComponent } from './component/transport/buses-page/bus-detail/bus-detail.component';
+import { ChangePasswordComponent } from './component/auth/change-password-page/change-password.component';
+import { ProfilePageComponent } from './component/user/profile-page/profile-page.component';
+import { ResetPasswordComponent } from './component/auth/reset-password-page/reset-password.component';
+import { MisPasajesComponent } from './component/ticket/mis-pasajes-page/mis-pasajes.component';
+import { ForgotPasswordComponent } from './component/auth/forgot-password-page/forgot-password.component';
 
 export const routes: Routes = [
+  { path: '', redirectTo: '/omnibus', pathMatch: 'full' },
   { path: 'login', component: LoginPageComponent },
-  { path: 'verificar-codigo', component: VerificarCodigoComponent },
+  { path: 'verify-reset-code', component: VerifyResetCodeComponent },
+  { path: 'forgot-password', component: ForgotPasswordComponent },
   { path: 'registro', component: SignupPageComponent },
 
-
-  { path: '', component: UsersPageComponent },
+  { path: 'usuarios', component: UsersPageComponent },
   { path: 'omnibus/:id', component: BusDetailComponent },
   { path: 'omnibus', component: BusesPageComponent },
-  { path: '', redirectTo: '/omnibus', pathMatch: 'full' },
   { path: 'localidades', component: LocalidadesPageComponent },
   { path: 'viajes', component: ViajesPageComponent },
+  { path: 'perfil/historial', component: ProfilePageComponent },
+  { path: 'mis-pasajes', component: MisPasajesComponent },
+  { path: 'change-password', component: ChangePasswordComponent },
+  { path: 'restablecer-contrasenia', component: ResetPasswordComponent },
 
 
-
-
-
-  { path: '**', redirectTo: '' },
+  
+  { path: '**', redirectTo: '' }
 ];

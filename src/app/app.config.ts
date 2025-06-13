0881095047f6886ca3789dcc29ae1765829f4136
bentanic-0 <bentanico@gmail.com>
2025-06-13 17:35:56 -0300

    Add SharedModule and MaterialModule to appConfig for improved modularity

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,16 +6,23 @@ import {
   withInterceptorsFromDi,
   HTTP_INTERCEPTORS
 } from '@angular/common/http';
+
 import { routes } from './app.routes';
 import { AuthInterceptor } from './core/auth/auth.interceptor';
 import { LoginService } from './services/login.service';
 import { MatPaginatorIntl } from '@angular/material/paginator';
 import { SpanishPaginatorIntl } from './shared/spanish-paginator-intl';
+import { SharedModule } from './shared/shared.module';
+import { MaterialModule } from './shared/material.module';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    importProvidersFrom(BrowserAnimationsModule),
+    importProvidersFrom(
+      BrowserAnimationsModule,
+      SharedModule,
+      MaterialModule
+    ),
     provideHttpClient(withInterceptorsFromDi()),
     {
       provide: HTTP_INTERCEPTORS,

b9e25c18272512188110e66aba5f97cf43f1007a
bentanic-0 <bentanico@gmail.com>
2025-06-16 16:55:40 -0300

    editarEstadoBus feature

---
+++ src/app/component/transport/buses-page/dialogs/confirm-dialog/confirm-dialog.component.ts
@@ -0,0 +1,32 @@
+import { ConfirmDialogComponent } from '../../../viajes-page/alta-viaje-expreso/dialogs/confirm-warning-dialog.component';
+import { Component, Inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatDialogModule, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button';
+
+export interface ConfirmBusDialogComponent {
+    title: string;
+    message: string;
+}
+
+@Component({
+    standalone: true,
+    selector: 'app-confirm-bus-dialog',
+    imports: [CommonModule, MatDialogModule, MatButtonModule],
+    templateUrl: './confirm-dialog.component.html',
+    styleUrl: './confirm-dialog.component.scss'
+})
+export class ConfirmBusDialogComponent {
+    constructor(
+        private dialogRef: MatDialogRef<ConfirmBusDialogComponent>,
+        @Inject(MAT_DIALOG_DATA) public data: ConfirmBusDialogComponent
+    ) { }
+
+    confirm(): void {
+        this.dialogRef.close(true);
+    }
+
+    cancel(): void {
+        this.dialogRef.close(false);
+    }
+}
--- src/app/models/buses/bus-dto.model.ts
+++ src/app/models/buses/bus-dto.model.ts
@@ -1,16 +1,25 @@
+export interface AsientoDto {
+  id: number;
+  numero: number;
+  estado: string;
+  omnibusId: number;
+}
+
 export interface BusDto {
   id: number;
   matricula: string;
   capacidad: number;
-  ubicacionActual: string;
   activo: boolean;
+  asientos: AsientoDto[];
 }
+
 
 export interface AltaBusDto {
   matricula: string;
   cantidadAsientos: number;
   localidadId: number;
 }
+
 
 export interface FiltroBusquedaBusDto {
   matricula?: string;
@@ -20,18 +29,4 @@ export interface FiltroBusquedaBusDto {
   localidadId?: number;
   fechaHoraSalida?: string;
   fechaHoraLlegada?: string;
-}
-
-export interface OmnibusDisponibleDto {
-  id: number;
-  matricula: string;
-  capacidad: number;
-  ubicacionActual: string;
-}
-
-export interface FiltroDisponibilidadOmnibusDto {
-  origenId: number;
-  destinoId: number;
-  fechaHoraSalida: string;
-  fechaHoraLlegada: string;
 }

152ea9532d92a370d5218e5da76d58e21d4335c0
miquel <miquelservian@gmail.com>
2025-06-21 16:10:18 -0300

    fix crear viajes resumen

--- src/app/component/viajes-page/dialogs/alta-viaje/alta-viaje-details/alta-viaje-details-dialog.component.ts
+++ src/app/component/viajes-page/dialogs/alta-viaje/alta-viaje-details/alta-viaje-details-dialog.component.ts
@@ -210,7 +210,6 @@ export class AltaViajeDetailsDialogComponent implements OnInit {
     });
   }
 
-  /** Devuelve un Date que combina fechaSalida + horaSalida */
   get salidaCompleta(): Date | null {
     if (!this.fechaSalida || !this.horaSalida) return null;
     const [hh, mm] = this.horaSalida.split(':').map(n => Number(n));
@@ -219,7 +218,6 @@ export class AltaViajeDetailsDialogComponent implements OnInit {
     return d;
   }
 
-  /** Devuelve un Date que combina fechaLlegada + horaLlegada */
   get llegadaCompleta(): Date | null {
     if (!this.fechaLlegada || !this.horaLlegada) return null;
     const [hh, mm] = this.horaLlegada.split(':').map(n => Number(n));

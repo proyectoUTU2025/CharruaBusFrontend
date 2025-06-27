5321b3259cc87a29fdc86769a8a4009cc15cbc38
miquel <miquelservian@gmail.com>
2025-06-27 15:39:36 -0300

    detalle pasaje arreglos

--- src/app/component/compra-detalle/compra-detalle-page.component.ts
+++ src/app/component/compra-detalle/compra-detalle-page.component.ts
@@ -1,3 +1,4 @@
+import { PasajeDto } from './../../models/pasajes/pasaje-dto.model';
 import { Component, OnInit } from '@angular/core';
 import { DetalleCompraDto } from '../../models';
 import { ActivatedRoute, Router, RouterModule } from '@angular/router';
@@ -10,6 +11,7 @@ import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { AuthService } from '../../services/auth.service';
+import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'app-compra-detalle-page',
@@ -22,7 +24,7 @@ import { AuthService } from '../../services/auth.service';
     MatPaginatorModule,
     MatButtonModule,
     RouterModule,
-    MatIconModule    
+    MatIconModule        
   ],
  templateUrl: './compra-detalle-page.component.html',
   styleUrls: ['./compra-detalle-page.component.scss']
@@ -31,7 +33,11 @@ export class CompraDetallePageComponent implements OnInit {
   compraId!: number;
   detalle!: DetalleCompraDto;
   isCliente = false;
-  mostrarBannerExito = false;
+  //mostrarBannerExito = false;
+  clienteNombre = '';
+  vendedorNombre = '';
+  ;
+
   displayedColumns = [
     'numeroAsiento',
     'paradaOrigen',
@@ -46,16 +52,29 @@ export class CompraDetallePageComponent implements OnInit {
     private route: ActivatedRoute,
     private router: Router,  
     private compraService: CompraService,
-    private authService: AuthService
+    private authService: AuthService,
+    private userService: UserService // Asumiendo que AuthService tiene el método getById
   ) {}
 
   ngOnInit() {
-    const navigation = this.router.getCurrentNavigation();
-    this.mostrarBannerExito = !!navigation?.extras.state?.['compraExitosa'];
+    //const navigation = this.router.getCurrentNavigation();
+    //this.mostrarBannerExito = !!navigation?.extras.state?.['compraExitosa'];
     this.isCliente = this.authService.rol === 'CLIENTE';
     this.compraId = Number(this.route.snapshot.paramMap.get('id'));
     this.compraService.getDetalle(this.compraId)
-      .subscribe(d => this.detalle = d);
+      .subscribe(d => {
+        this.detalle = d;
+        // cargar nombre de cliente
+        this.userService.getById(d.clienteId)
+          .then(u => this.clienteNombre = `${u.nombre} ${u.apellido}`)
+          .catch(() => this.clienteNombre = '');
+        // si vino de un vendedor, cargar nombre del vendedor
+        if (!this.isCliente) {
+          this.userService.getById(d.vendedorId!)
+            .then(v => this.vendedorNombre = `${v.nombre} ${v.apellido}`)
+            .catch(() => this.vendedorNombre = '');
+        }
+      });
   }
 
   descargarPdf() {
@@ -64,5 +83,8 @@ export class CompraDetallePageComponent implements OnInit {
         const url = URL.createObjectURL(blob);
         window.open(url, '_blank');
       });
+  }
+  abrirDetallePasaje(PasajeDto: PasajeDto) {
+    this.router.navigate(['/pasaje']);
   }
 }

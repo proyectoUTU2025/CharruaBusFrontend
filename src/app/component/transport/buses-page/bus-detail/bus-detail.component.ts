b06a1e1d76a45a7d4572ff709f234da728dbc03a
bentanic-0 <bentanico@gmail.com>
2025-07-01 15:24:32 -0300

    refactor(bus-detail): simplify bus loading logic and remove unused components

--- src/app/component/transport/buses-page/bus-detail/bus-detail.component.ts
+++ src/app/component/transport/buses-page/bus-detail/bus-detail.component.ts
@@ -1,15 +1,11 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { MatDialog } from '@angular/material/dialog';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { SharedModule } from '../../../../shared/shared.module';
 import { MaterialModule } from '../../../../shared/material.module';
 import { NgIf } from '@angular/common';
 import { BusService } from '../../../../services/bus.service';
 import { BusDto } from '../../../../models/buses';
-import { OmnibusHistoryComponent } from './omnibus-history/omnibus-history.component';
-import { AsignarMantenimientoComponent } from '../../mantenimientos-page/asignar-mantenimiento/asignar-mantenimiento.component';
-import { AltaViajeExpresoComponent } from '../../viajes-page/alta-viaje-expreso/alta-viaje-expreso.component';
 
 @Component({
     standalone: true,
@@ -19,66 +15,47 @@ import { AltaViajeExpresoComponent } from '../../viajes-page/alta-viaje-expreso/
         MaterialModule,
         NgIf,
         MatProgressSpinnerModule,
-        OmnibusHistoryComponent,
-        AsignarMantenimientoComponent,
-        AltaViajeExpresoComponent
     ],
     templateUrl: './bus-detail.component.html',
     styleUrls: ['./bus-detail.component.scss']
 })
 export class BusDetailComponent implements OnInit {
-    busId!: number;
+    @Input() busId?: number;
     bus?: BusDto;
     loading = true;
     error = '';
-    @ViewChild(OmnibusHistoryComponent) historyComponent!: OmnibusHistoryComponent;
 
     constructor(
-        private route: ActivatedRoute,
         private busService: BusService,
-        private dialog: MatDialog
+        private route: ActivatedRoute
     ) { }
 
     ngOnInit() {
-        this.route.paramMap.subscribe(params => {
-            const id = params.get('id');
-            if (id) {
-                this.busId = +id;
+        if (this.busId) {
+            this.loadBus();
+        } else {
+            const paramId = this.route.snapshot.paramMap.get('id');
+            if (paramId) {
+                this.busId = +paramId;
                 this.loadBus();
+            } else {
+                this.error = 'No se encontró el ID del ómnibus en la URL.';
+                this.loading = false;
             }
-        });
+        }
     }
 
     private loadBus() {
+        if (!this.busId) {
+            this.error = 'No se especificó ómnibus.';
+            this.loading = false;
+            return;
+        }
         this.loading = true;
         this.error = '';
         this.busService.getById(this.busId).subscribe({
             next: b => { this.bus = b; this.loading = false; },
             error: () => { this.error = 'No se pudo cargar detalle'; this.loading = false; }
-        });
-    }
-
-    abrirAltaViajeExpreso() {
-        const ref = this.dialog.open(AltaViajeExpresoComponent, {
-            width: '600px', data: { omnibusId: this.busId }
-        });
-        ref.afterClosed().subscribe(res => {
-            if (res === true || res === 'viajeRegistrado') {
-                this.loadBus();
-                setTimeout(() => this.historyComponent.loadHistory(), 0);
-            }
-        });
-    }
-
-    abrirAsignarMantenimiento() {
-        const ref = this.dialog.open(AsignarMantenimientoComponent, {
-            width: '600px', data: { omnibusId: this.busId }
-        });
-        ref.afterClosed().subscribe(res => {
-            if (res === 'mantenimientoCreado') {
-                this.loadBus();
-                setTimeout(() => this.historyComponent.loadHistory(), 0);
-            }
         });
     }
 }

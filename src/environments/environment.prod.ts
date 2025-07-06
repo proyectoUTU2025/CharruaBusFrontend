c1c02ef035ec0659bb2b6b081846eb701fd9df93
Facundo Alvarez de Ron <facundo.alvarezderon@gmail.com>
2025-07-06 20:26:38 -0300

    Commit Changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  //apiBaseUrl: 'https://charruabusbackend-production.up.railway.app' 
+  apiBaseUrl: 'https://charruabusbackend-production.up.railway.app'
 };
